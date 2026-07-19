455d4b46f02490e763444255a643b28296452ba1
sina <sina.tabriziyan@gmail.com>
2026-07-20 01:09:03 +0330

    init adaptive

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.material3.adaptive)
+
 }
